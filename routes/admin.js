7ad0fa2db763bdaf9ed0fd30e2841deee1860e84
YogiK2001 <kaustubh7718@gmail.com>
2024-10-12 18:35:23 +0530

    completed signup auth

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,10 @@
 const { Router, application } = require('express');
 const adminRouter = Router();
-const { Admin } = require('../database/db');
+const { User, Course, Admin, Purchase } = require('../database/db');
 const { z } = require("zod");
+const jwt = require("jsonwebtoken");
+
+require('dotenv').config();
 
 app.use(expres.json());
 
@@ -13,7 +16,6 @@ async function generateHash(password) {
 
 adminRouter.post('/signup', async function (req, res) {
     try {
-
         const requiredResponse = z.object({
             email: z.string().min(3).max(100).email(),
             password: z.string().min(5).max(50),
@@ -25,17 +27,27 @@ adminRouter.post('/signup', async function (req, res) {
         if (parsedData.success) {
             const { email, password, firstName, lastName } = parsedData.data;
 
+            const existingAdmin = await Admin.findOne({ email });
+            if (existingAdmin) {
+                return res.status(409).json({ message: "Admin already exists" });
+            }
+
             let hash = await generateHash(password);
 
-            await Admin.create({
+            const newAdmin = await Admin.create({
                 email: email,
                 password: hash,
                 firstName: firstName,
                 lastName: lastName
             })
 
-            res.json({
-                message: "Congratulations! You have signed up"
+            const token = jwt.sign({
+                id: newAdmin._id.toString()
+            }, process.env.JWT_SECRET)
+
+            res.status(200).json({
+                message: "Congratulations! You have signed up",
+                token: token
             })
         } else {
             res.status(403).json({
